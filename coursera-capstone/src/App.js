e040c2c9bff79452b222e6eb7eb329a1d44c110f
tandugonkhalid <khalidtandugon@gmail.com>
2026-01-23 11:09:01 +0300

    Added Components & add meta tags in index.html

--- coursera-capstone/src/App.js
+++ coursera-capstone/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ coursera-capstone/src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+return (
+    <div>
+        <p>asdf</p>
+    </div>
+
+);
+
+}
+
+export default Header;
