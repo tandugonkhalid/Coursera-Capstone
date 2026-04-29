b66638dc5020d5c7379d6d55b4e4cbd9c9fad7b6
tandugonkhalid <khalidtandugon@gmail.com>
2026-04-29 14:33:05 +0300

    Added css for Card.js

--- coursera-capstone/src/components/Card.js
+++ coursera-capstone/src/components/Card.js
@@ -5,46 +5,48 @@ import bgimage2 from '../assets/lemon dessert.jpg';
 
 function Card(){
     return(
-        <div id="card">
+        <div>
             <div id="card-header">
                 <h1>This weeks specials!</h1> 
                 <div id='reserve-btn'>
                     <a href="Menu">Online Menu</a>
                 </div>
             </div>
-            <div id="card-body">
-                <img src={bgimage} alt="background-image" />
-                <div id="card-title"> 
-                    <h6>Greek Salad</h6>
-                    <p>$ 12.99</p>
+            <div id="card">
+                <div class="card-body">
+                    <img class="card-img" src={bgimage} alt="background-image" />
+                    <div id="card-title"> 
+                        <h4>Greek Salad</h4>
+                        <p>$ 12.99</p>
+                    </div>
+                    <p class="card-desc">
+                        The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, 
+                        garnished with crucnhy garlic and rosemary croutons.
+                    </p>
+                    <div class="card-links"><a href="Delivery">Order a delivery</a> </div>
                 </div>
-                <p>
-                    The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, 
-                    garnished with crucnhy garlic and rosemary croutons.
-                </p>
-                <a href="Delivery">Order a delivery</a>
-            </div>
-            <div id="card-body">
-                <img src={bgimage1} alt="background-image" />
-                <div id="card-title"> 
-                    <h6>Bruschetta</h6>
-                    <p>$ 5.99</p>
+                <div class="card-body">
+                    <img class="card-img" src={bgimage1} alt="background-image" />
+                    <div id="card-title"> 
+                        <h4>Bruschetta</h4>
+                        <p>$ 5.99</p>
+                    </div>
+                    <p class="card-desc">
+                        Our Bruschetta is made from grilled bread that has been semared with garlic and seasoned with salt and olive oil.
+                    </p>
+                    <div class="card-links"><a href="Delivery">Order a delivery</a> </div>
                 </div>
-                <p>
-                    Our Bruschetta is made from grilled bread that has been semared with garlic and seasoned with salt and olive oil.
-                </p>
-                <a href="Delivery">Order a delivery</a>
-            </div>
-            <div id="card-body">
-                <img src={bgimage2} alt="background-image" />
-                <div id="card-title"> 
-                    <h6>Lemon Dessert</h6>
-                    <p>$ 5.00</p>
+                <div class="card-body">
+                    <img class="card-img" src={bgimage2} alt="background-image" />
+                    <div id="card-title"> 
+                        <h4>Lemon Dessert</h4>
+                        <p>$ 5.00</p>
+                    </div>
+                    <p class="card-desc">
+                        This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
+                    </p>
+                    <div class="card-links"><a href="Delivery">Order a delivery</a> </div>
                 </div>
-                <p>
-                    This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
-                </p>
-                <div><a href="Delivery">Order a delivery</a> </div>
             </div>
         </div>
     );
